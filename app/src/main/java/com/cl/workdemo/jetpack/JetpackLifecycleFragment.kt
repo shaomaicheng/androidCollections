b15d641997e36f670d7299e866eecf4525d05fea
shaomaicheng <devchenglei@gmail.com>
2020-07-12 11:31:19 +0800

    插拔一个view，view在被动态拔出的时候走destroy的销毁逻辑

--- app/src/main/java/com/cl/workdemo/jetpack/JetpackLifecycleFragment.kt
+++ app/src/main/java/com/cl/workdemo/jetpack/JetpackLifecycleFragment.kt
@@ -1,14 +1,20 @@
 package com.cl.workdemo.jetpack
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.findFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.*
 import com.cl.workdemo.R
@@ -27,6 +33,24 @@ class JetpackLifecycleFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentJetpackLifecycleBinding.inflate(inflater, container, false)
+        val lifecycleOb = LifecycleEventObserver { source, event ->
+            Log.e("lifecycleEventObserver", event.toString())
+            when (event) {
+                Lifecycle.Event.ON_DESTROY -> {
+                    // 销毁逻辑
+                    for (index in 0 until binding.stubContainer.childCount) {
+                        val child = binding.stubContainer[index]
+                        if (child is StubView) {
+                            child.release()
+                        }
+                    }
+                }
+            }
+        }
+        val stubVH = StubViewHolder(this, binding).apply {
+            addLifecycleObserver(lifecycleOb)
+        }
+        lifecycle.addObserver(lifecycleOb)
         jetpackLifecycleVM =  ViewModelProvider(this)[JetpackLifecycleViewModel::class.java]
 
         binding.lifecycleOwner = this
@@ -35,7 +59,14 @@ class JetpackLifecycleFragment:Fragment() {
         binding.clicker = View.OnClickListener {
             when (it.id) {
                 R.id.btnInstallOrUninstall -> {
-
+                    if (jetpackLifecycleVM.installed.value == true) {
+                        // 拔掉
+                        stubVH.unInstall()
+                    } else {
+                        // 插入
+                        stubVH.install()
+                    }
+                    jetpackLifecycleVM.installed.value = !(jetpackLifecycleVM.installed.value ?: false)
                 }
             }
         }
@@ -57,25 +88,64 @@ class StubView : LinearLayout {
         defStyleAttr
     )
 
-    constructor(
-        context: Context?,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
+    fun release() {
+        Log.e("StubView", "release")
+    }
 
-    private var lifecycleOwner : LifecycleOwner = LifecycleOwnerWrapper(context as FragmentActivity)
-
-    
-
+    init {
+        val tv = TextView(context).apply {
+            text= "占位控件"
+            gravity = Gravity.CENTER
+            setBackgroundColor(Color.BLUE)
+            setTextColor(Color.WHITE)
+        }
+        this.addView(tv)
+        val lp = tv.layoutParams
+        lp.width= ViewGroup.LayoutParams.MATCH_PARENT
+        lp.height = 40
+        tv.layoutParams= lp
+    }
 }
 
-class LifecycleOwnerWrapper(val owner: LifecycleOwner) : LifecycleOwner {
+class StubViewHolder(val owner: Fragment, val binding: FragmentJetpackLifecycleBinding) {
+    private var lifecycleOwner : LifecycleOwner = LifecycleOwnerWrapper(owner)
+    private val TAG = this::class.java.simpleName
+
+    fun addLifecycleObserver(ob: LifecycleObserver) {
+        (lifecycleOwner as LifecycleOwnerWrapper).addObserver(ob)
+    }
+
+    fun install() {
+        val stub = StubView(owner.context)
+        (lifecycleOwner as LifecycleOwnerWrapper).markState(Lifecycle.State.STARTED)
+        binding.stubContainer.addView(stub)
+    }
+
+    fun unInstall() {
+        for (index in 0 until binding.stubContainer.childCount) {
+            val child = binding.stubContainer[index]
+            if (child is StubView) {
+                (lifecycleOwner as LifecycleOwnerWrapper).markState(Lifecycle.State.DESTROYED)
+                binding.stubContainer.removeView(child)
+            }
+        }
+    }
+}
+
+class LifecycleOwnerWrapper(owner: LifecycleOwner) : LifecycleOwner {
 
     private var lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(owner)
 
     override fun getLifecycle(): Lifecycle {
         return lifecycleRegistry
+    }
+
+    fun markState(state: Lifecycle.State) {
+        lifecycleRegistry.currentState = state
+    }
+
+    fun addObserver(ob: LifecycleObserver) {
+        lifecycleRegistry.addObserver(ob)
     }
 
 }
